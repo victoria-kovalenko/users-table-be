2169d5a539971df76c0c650429182b88106df6bc
Victoria Kovalenko <v.kovalenkoooooo@gmail.com>
2023-03-27 14:30:35 +0300

    get by id method

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import cors from 'cors';
+import { ObjectId } from 'mongodb';
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://victoriakovalenkojob:Vv0820132525@cluster0.svj5c27.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect((err: any) => {
   const collection = client.db("thinkmobiles_tt").collection("users");
-  // perform actions on the collection object
   if (!err) {
     console.log('connect');
   } else {
@@ -19,15 +19,9 @@ const app = express();
 
 app.use(cors());
 
-app.get('/', (req: any, res: any) => {
+app.get('/users', (req: any, res: any) => {
   client.connect((err: any) => {
     const collection = client.db("thinkmobiles_tt").collection("users");
-    if (!err) {
-      console.log('connect');
-    } else {
-      console.log(err);
-    }
-
     collection.find({}).toArray(function (err, docs) {
       console.log("Found the following records");
       res.send(docs);
@@ -36,6 +30,20 @@ app.get('/', (req: any, res: any) => {
     });
   });
 });
+
+app.get('/users/:userId', (req: any, res: any) => {
+  const { userId } = req.params;
+
+  client.connect((err: any) => {
+    const collection = client.db("thinkmobiles_tt").collection("users");
+    collection.find({_id: ObjectId(userId)}).toArray(function (err, docs) {
+      console.log("Found the following record");
+      res.send(docs);
+
+      res.end();
+    });
+  });
+})
 
 app.post('/users', express.json(), async (req: any, res: any) => {
   const data = {
@@ -49,15 +57,7 @@ app.post('/users', express.json(), async (req: any, res: any) => {
 
   client.connect((err: any) => {
     const collection = client.db("thinkmobiles_tt").collection("users");
-    if (!err) {
-      console.log('connect');
-    } else {
-      console.log(err);
-    }
-
-     const db = client.db('thinkmobiles_tt');
-
-    db.collection('users').insertOne(data, (err: any, result: any) => {
+    collection.insertOne(data, (err: any, result: any) => {
     if (err) {
       res.send({ 'error': 'An error has occurred' });
 
